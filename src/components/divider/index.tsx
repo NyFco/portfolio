0faca3179f94e4a6afafd987e3191f240fca1474
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2023-04-09 17:10:12 +0430

    Add divider component

---
+++ src/components/divider/index.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+
+/**
+ * size should be between 0 and 100
+ * @returns Vertical/Horizontal divider
+ */
+const Divider: FC<{ direction?: 'vertical' | 'horizontal'; size?: number }> = ({
+  direction = 'horizontal',
+  size = 70,
+}) => {
+  const verifiedSize = size > 100 || size < 0 ? 70 : size;
+  return (
+    <div
+      style={{
+        padding:
+          direction === 'horizontal'
+            ? `0 ${(100 - verifiedSize) / 2}%`
+            : `${(100 - verifiedSize) / 2}% 0`,
+      }}
+    >
+      <div
+        style={{
+          height: direction === 'horizontal' ? '2px' : '100%',
+          width: direction === 'horizontal' ? '100%' : '2px',
+          backgroundColor: '#ffffff',
+        }}
+      />
+    </div>
+  );
+};
+
+export default Divider;
