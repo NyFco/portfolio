182e6730b11946c0a00b282862edc51ae74a2a31
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2023-04-09 17:10:35 +0430

    Add and config react-intl

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { IntlProvider } from 'react-intl';
 
 import App from './App';
+import { English } from './lang';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <IntlProvider locale="en" messages={English}>
+      <App />
+    </IntlProvider>
   </React.StrictMode>
 );
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import { FC } from 'react';
+import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
 
-const MainPage = () => {
-  return <div>MainPage</div>;
+import { Divider } from '../../components';
+
+const Layout = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+});
+
+const NameContainer = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  textAlign: 'center',
+});
+
+const MainPage: FC = () => {
+  return (
+    <Layout>
+      <NameContainer>
+        <h1>
+          <FormattedMessage id="name.firstname" />
+        </h1>
+        <h2>
+          <FormattedMessage id="name.lastname" />
+        </h2>
+        <h2>
+          <FormattedMessage id="frontendev" />
+        </h2>
+        <Divider direction="horizontal" />
+      </NameContainer>
+    </Layout>
+  );
 };
 
 export default MainPage;
