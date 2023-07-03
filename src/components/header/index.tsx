1eab1a49cc18aa2e3423fc99de879cf21513455c
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2023-07-03 12:57:14 +0330

    Add Links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Link = styled.h3({
-  fontWeight: 100,
-  color: '#ccc',
-});
+const Link = styled.h3(
+  {
+    fontWeight: 100,
+    color: '#ccc',
+    cursor: 'pointer',
+  },
+  () => `
+&:hover {
+  color: #fff;
+  transition: ease 0.3s all
+}
+`
+);
+
+const Name = styled.h2(
+  { display: 'flex', gap: 5, cursor: 'pointer' },
+  () => `
+  &:hover {
+    color: #ccc;
+    transition: ease 0.3s all
+  }
+`
+);
+
+const LastName = styled.span(
+  ({ theme }) => `
+  display: none;
+  ${theme.breakpoints.sm} {
+    display: block;
+  }
+`
+);
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <header
       style={{
@@ -18,16 +48,36 @@ const Header: React.FC = () => {
         boxShadow: '0 0 32px 0 #aaa',
       }}
     >
-      <h2>
-        <FormattedMessage id="name.fullname" />
-      </h2>
-      <Link style={{ fontWeight: 100 }}>
+      <Name
+        onClick={() => {
+          navigate('/');
+        }}
+      >
+        <FormattedMessage id="name.firstname" />
+        <LastName>
+          <FormattedMessage id="name.lastname" />
+        </LastName>
+      </Name>
+      <Link
+        onClick={() => {
+          navigate('/about');
+        }}
+        style={{ fontWeight: 100 }}
+      >
         <FormattedMessage id="about" />
       </Link>
-      <Link>
+      <Link
+        onClick={() => {
+          navigate('/projects');
+        }}
+      >
         <FormattedMessage id="projects" />
       </Link>
-      <Link>
+      <Link
+        onClick={() => {
+          navigate('/contact');
+        }}
+      >
         <FormattedMessage id="contact" />
       </Link>
     </header>
